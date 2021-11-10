3562f2855116ab08e3551a9db72abe0f9056c933
Kaiser Slocum <slocumkaiser@gmail.com>
2021-11-10 11:54:41 -0800

    working on js

--- flask_app/draw/static/index.js
+++ flask_app/draw/static/index.js
@@ -34,106 +34,127 @@ function encodeImageFileAsURL(element){
 }
 
 //source: https://stackoverflow.com/questions/2368784/draw-on-html5-canvas-using-a-mouse
-    var canvas, ctx, flag = false,
-        prevX = 0,
-        currX = 0,
-        prevY = 0,
-        currY = 0,
-        dot_flag = false;
+// Javascript for Canvas design
+var canvas, ctx, flag = false,
+    prevX = 0,
+    currX = 0,
+    prevY = 0,
+    currY = 0,
+    dot_flag = false;
+const ary = [];
+ary.push("=");
 
-    var x = "black",
-        y = 2;
+var x = "black", y = 2;
 
-    window.onresize = windowResized;
+window.onresize = windowResized;
 
-    function windowResized() {
-        canvas = document.getElementById('can');
-        cont = document.getElementById('canvas');
-        canvas.width = cont.clientWidth / 1.03  ;
-        canvas.height = cont.clientHeight / 1.5;
-    }
+function windowResized() 
+{
+    canvas = document.getElementById('can');
+    cont = document.getElementById('canvas');
+    canvas.width = cont.clientWidth / 1.03  ;
+    canvas.height = cont.clientHeight / 1.5;
+}
     
-    function init() {
-        canvas = document.getElementById('can');
-        windowResized();
+function init() 
+{
+    canvas = document.getElementById('can');
+    windowResized();
 
-        ctx = canvas.getContext("2d");
-        w = canvas.width;
-        h = canvas.height;
+    ctx = canvas.getContext("2d");
+    w = canvas.width;
+    h = canvas.height;
     
-        canvas.addEventListener("mousemove", function (e) {
-            findxy('move', e)
-        }, false);
-        canvas.addEventListener("mousedown", function (e) {
-            findxy('down', e)
-        }, false);
-        canvas.addEventListener("mouseup", function (e) {
-            findxy('up', e)
-        }, false);
-        canvas.addEventListener("mouseout", function (e) {
-            findxy('out', e)
-        }, false);
+    canvas.addEventListener("mousemove", function (e) { findxy('move', e) }, false);
+    canvas.addEventListener("mousedown", function (e) { findxy('down', e) }, false);
+    canvas.addEventListener("mouseup", function (e) { findxy('up', e) }, false);
+    canvas.addEventListener("mouseout", function (e) { findxy('out', e) }, false);
+
+    displayCanvas();
+}
+    
+function color(obj) 
+{
+    x = obj.id;
+    if (x == "white") 
+        y = 14;
+    else 
+        y = 2;    
+}
+    
+function draw() 
+{
+    ctx.beginPath();
+    ctx.moveTo(prevX, prevY);
+    ctx.lineTo(currX, currY);
+    ctx.strokeStyle = x;
+    ctx.lineWidth = y;
+    ctx.stroke();
+    ctx.closePath();
+}
+    
+function erase() 
+{
+    var m = confirm("Want to clear");
+    if (m) 
+    {
+        ctx.clearRect(0, 0, w, h);
+        document.getElementById("canvasimg").style.display = "none";
     }
+    displayCanvas();
+}
     
-    function color(obj) {
-        x = obj.id;
-        if (x == "white") 
-            y = 14;
-        else y = 2;    
-    }
+function displayCanvas()
+{
+    document.getElementById("canvasimg").style.border = "2px solid";
+    var dataURL = canvas.toDataURL();
+    document.getElementById("canvasimg").src = dataURL;
+    document.getElementById("canvasimg").style.display = "inline";
+}
+function save() 
+{
+    displayCanvas();
+
+    var dataURL = canvas.toDataURL();
+    var thing = document.getElementById("displayEquation");
+    ary.push(translate_to_char(dataURL));
+    thing.innerText = ary;
+}
     
-    function draw() {
-        ctx.beginPath();
-        ctx.moveTo(prevX, prevY);
-        ctx.lineTo(currX, currY);
-        ctx.strokeStyle = x;
-        ctx.lineWidth = y;
-        ctx.stroke();
-        ctx.closePath();
-    }
+function findxy(res, e) 
+{
+    if (res == 'down') 
+    {
+        prevX = currX;
+        prevY = currY;
+        currX = e.clientX - canvas.offsetLeft;
+        currY = e.clientY - canvas.offsetTop;
     
-    function erase() {
-        var m = confirm("Want to clear");
-        if (m) {
-            ctx.clearRect(0, 0, w, h);
-            document.getElementById("canvasimg").style.display = "none";
+        flag = true;
+        dot_flag = true;
+        if (dot_flag) 
+        {
+            ctx.beginPath();
+            ctx.fillStyle = x;
+            ctx.fillRect(currX, currY, 2, 2);
+            ctx.closePath();
+            dot_flag = false;
         }
     }
-    
-    function save() {
-        document.getElementById("canvasimg").style.border = "2px solid";
-        var dataURL = canvas.toDataURL();
-        document.getElementById("canvasimg").src = dataURL;
-        document.getElementById("canvasimg").style.display = "inline";
+    if (res == 'up' || res == "out")
+        flag = false;
+    if ((res == 'move') && (flag))
+    {
+        prevX = currX;
+        prevY = currY;
+        currX = e.clientX - canvas.offsetLeft;
+        currY = e.clientY - canvas.offsetTop;
+        draw();
     }
-    
-    function findxy(res, e) {
-        if (res == 'down') {
-            prevX = currX;
-            prevY = currY;
-            currX = e.clientX - canvas.offsetLeft;
-            currY = e.clientY - canvas.offsetTop;
-    
-            flag = true;
-            dot_flag = true;
-            if (dot_flag) {
-                ctx.beginPath();
-                ctx.fillStyle = x;
-                ctx.fillRect(currX, currY, 2, 2);
-                ctx.closePath();
-                dot_flag = false;
-            }
-        }
-        if (res == 'up' || res == "out") {
-            flag = false;
-        }
-        if (res == 'move') {
-            if (flag) {
-                prevX = currX;
-                prevY = currY;
-                currX = e.clientX - canvas.offsetLeft;
-                currY = e.clientY - canvas.offsetTop;
-                draw();
-            }
-        }
-    }
+}
+
+
+function translate_to_char(image)
+{
+    return "t";
+}
