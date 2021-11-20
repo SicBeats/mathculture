73a63938fd8d4720c29f8ccd459817246a023253
Kaiser Slocum <slocumkaiser@gmail.com>
2021-11-19 21:40:25 -0800

    more edits on blacklist js

--- flask_app/auth/static/blacklist.js
+++ flask_app/auth/static/blacklist.js
@@ -7,11 +7,10 @@ Team: Map Culture (Team 5)
 
 Last Modified: 11/18/2021
 */
-var arrGlob = new Array();
-var newArr = new Array();
+
 /***********************************************************************************************
 FUNCTION: loadfile
-PURPOSE: This function takes the uploaded file image and displays it in the canvas image element
+PURPOSE: This function takes the uploaded text file and calls displayUploadedEquations()
 SOURCE: https://stackoverflow.com/questions/14446447/how-to-read-a-local-text-file
 ************************************************************************************************/
 function loadfile(event) 
@@ -33,47 +32,58 @@ function loadfile(event)
     }
     reader.readAsText(input.files[0]);   
 }
+/***********************************************************************************************
+FUNCTION: displayUploadedEquations
+PURPOSE: This function organizes all the uploaded equations into an array of equations to be sent to the db
+************************************************************************************************/
 function displayUploadedEquations()
-{
+{    
+    var charArr = new Array();
+    var equatArr = new Array();
+
     // We need to check for invalid equations!
     var node = document.getElementById('uploaded_equations');
     //var box = document.getElementById('equation_input');
     let arr = node.innerText;
-    arrGlob = arr.split("");
+    charArr = arr.split("");
     node.innerText = "";
     let equat_num = 1;    
     
     // Remove \n if at beginning!
-    for (let i = 0; i < arrGlob.length; i++)
+    for (let i = 0; i < charArr.length; i++)
     {
-        if (((arrGlob[i] == '\n') && ((arrGlob[i+1] == '\n') || (arrGlob[i-1] == '\n'))) ||
-        ((arrGlob[i] != ' ') && (arrGlob[i] != '\n') && (is_valid_char(arrGlob[i]) == false)))
+        if (((charArr[i] == '\n') && ((charArr[i+1] == '\n') || (charArr[i-1] == '\n'))) ||
+        ((charArr[i] != ' ') && (charArr[i] != '\n') && (is_valid_char(charArr[i]) == false)))
         {
-            arrGlob.splice(i,1);
+            charArr.splice(i,1);
             i--
         }   
     }
 
     var ic = 0;
-    newArr[ic] = "";
-    for (let i = 0; i < arrGlob.length; i++)
+    equatArr[ic] = "";
+    for (let i = 0; i < charArr.length; i++)
     {
-        if (arrGlob[i] ==  '\n')
+        if (charArr[i] ==  '\n')
         {
             ic++;
-            newArr[ic] = "";
+            equatArr[ic] = "";
         } 
         else
         {
-            newArr[ic] += arrGlob[i];
+            equatArr[ic] += charArr[i];
         } 
     }
 
-    for (var i = 0; i < newArr.length; i++)
+    for (var i = 0; i < equatArr.length; i++)
     {
-        node.innerText += "\nEquation " + equat_num++ + ": " + newArr[i];
+        node.innerText += "\nEquation " + equat_num++ + ": " + equatArr[i];
     }
 }
+/***********************************************************************************************
+FUNCTION: is_valid_char
+PURPOSE: Checks if the argument is one of the recognized characters
+************************************************************************************************/
 function is_valid_char(char)
 {
     const arr = ["0","1","2","3","4","5","6","7","8","9","x","y","z","+","-","*","/","=","(",")"];
@@ -84,17 +94,27 @@ function is_valid_char(char)
     }
     return false;
 }
+/***********************************************************************************************
+FUNCTION: publish
+PURPOSE: Moves the inputted equation to the db
+************************************************************************************************/
 function publish()
 {
+    // Check to see all the characters are valid
     var equat = document.getElementById("equation_input").value;
     console.log(equat);
-    //add to blacklist
+    //publish to blacklist
 }
+/***********************************************************************************************
+FUNCTION: unpublish
+PURPOSE: Removes the inputted equation to the db
+************************************************************************************************/
 function unpublish()
 {
+    // Check to see all the characters are valid
     var equat = document.getElementById("equation_input").value;
     console.log(equat);
-    //remove from blacklist
+    //unpublish from blacklist
 }
 function publishAll()
 {
